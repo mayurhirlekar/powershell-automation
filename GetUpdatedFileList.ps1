7bce3818beaa84fc51e3cd5ff06840918f33656e
Mayur Hirlekar <mayur.hirlekar@americaneagle.com>
2018-04-04 13:49:39 -0500

    Code to get file list by comparing dates

---
+++ GetUpdatedFileList.ps1
@@ -0,0 +1,14 @@
+﻿ $tempFilePath =  ($env:TEMP + '\file.txt')
+ $updatesPath = Read-Host -Prompt 'Please enter updates path'
+ $numberOfDays = Read-Host -Prompt 'Number of Days before today to check'
+ $excludeFolder = 'tempIIS','assets'
+ if(Test-Path $updatesPath) { 
+ Get-ChildItem -Path $updatesPath -Exclude $excludeFolder -Recurse $fList -File  |
+  % -Process { $nowTime = Get-Date; if (($nowTime - $_.LastWriteTime).TotalDays -lt $numberOfDays) { $_.FullName}} |
+   Out-File $tempFilePath 
+ Notepad $tempFilePath } else {
+  Write-Host "Path is invalid"
+ }
+
+
+
